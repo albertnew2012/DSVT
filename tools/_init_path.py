34f588b9aa3bfc78a6ae1265d6a0689bb39df630
Gokul Hari <gokulh@safeai.ai>
2024-07-16 23:45:59 +0000

    update launch.json to add pwd env to sys.path

--- tools/_init_path.py
+++ tools/_init_path.py
@@ -1,4 +1,2 @@
 import sys
-sys.path.insert(0, '../')
-if '/home/ubuntu/adehome/safeai_ml/libraries/OpenPCDet' in sys.path:
-    sys.path.remove('/home/ubuntu/adehome/safeai_ml/libraries/OpenPCDet')
+# sys.path.insert(0, '../')
