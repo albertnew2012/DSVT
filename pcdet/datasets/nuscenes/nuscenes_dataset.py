6fea2dcd0f4e746a86273b3c58bb30e32ca7685f
Gokul Hari <gokulh@safeai.ai>
2024-08-13 16:02:18 -0700

    update nuscenes_dataset.py to make it possible to comment out 'timestamp' in nuscenes_dataset.yaml

--- pcdet/datasets/nuscenes/nuscenes_dataset.py
+++ pcdet/datasets/nuscenes/nuscenes_dataset.py
@@ -103,9 +103,9 @@ class NuScenesDataset(DatasetTemplate):
             sweep_times_list.append(times_sweep)
 
         points = np.concatenate(sweep_points_list, axis=0)
-        # times = np.concatenate(sweep_times_list, axis=0).astype(points.dtype)
-
-        # points = np.concatenate((points, times), axis=1)
+        if 'timestamp' in self.dataset_cfg["POINT_FEATURE_ENCODING"]["used_feature_list"]:
+            times = np.concatenate(sweep_times_list, axis=0).astype(points.dtype)
+            points = np.concatenate((points, times), axis=1)
         return points
 
     def __len__(self):
